cfd4cafadb35bc6486c634d768ecc1e54f194a81
Evan Voordeckers <evan.voordeckers@yahoo.com>
2022-01-13 11:21:48 -0700

    make file still returning error on assignment1.c compilation

--- Asg01/assignment1.c
+++ Asg01/assignment1.c
@@ -9,21 +9,11 @@
 #include <unistd.h>
 
 uint64_t byte_sort(uint64_t arg) {
-    uint8_t firstByte = (arg) & 0xff;
-    uint8_t secondByte = (arg >> 8) & 0xff;
-    uint8_t thirdByte = (arg >> 16) & 0xff;
-    uint8_t fourthByte = (arg >> 24) & 0xff;
-    uint8_t fifthByte = (arg >> 32) & 0xff;
-    uint8_t sixthByte = (arg >> 40) & 0xff;
-    uint8_t seventhByte = (arg >> 48) & 0xff;
-    uint8_t eighthByte = (arg >> 56) & 0xff;
-
-
-    return firstByte + secondByte + thirdByte + fourthByte + fifthByte + sixthByte + seventhByte + eighthByte;
+    return arg;
 }
 
 uint64_t nibble_sort(uint64_t arg) {
-    return 0;
+    return arg;
 }
 
 void convert(enum format_t mode, uint64_t value) {
--- Asg01/main.c
+++ Asg01/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("makefile works!");
+    printf("makefile works!\n");
     return 0;
 }
