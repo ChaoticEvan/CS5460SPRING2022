2ba38bdb98e2b61d1cba088ea702b358671527a1
Evan Voordeckers <evan.voordeckers@yahoo.com>
2022-01-12 19:19:32 -0700

    INIT COMMIT

---
+++ Asg01/assignment1.c
@@ -0,0 +1,25 @@
+#define _GNU_SOURCE
+
+#include "assignment1.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <sys/syscall.h> 
+#include <unistd.h>
+
+uint64_t byte_sort(uint64_t arg) {
+    return 0;
+}
+
+uint64_t nibble_sort(uint64_t arg) {
+    return 0;
+}
+
+void convert(enum format_t mode, uint64_t value) {
+
+}
+
+void draw_u(void) {
+
+}
