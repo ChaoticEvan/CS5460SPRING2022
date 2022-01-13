919263bf0dbc28d759435901217f7dd88d8948ba
Evan Voordeckers <evan.voordeckers@yahoo.com>
2022-01-12 22:03:34 -0700

    Cannot build into executable... Errors not helpful

--- Asg01/assignment1.c
+++ Asg01/assignment1.c
@@ -8,8 +8,24 @@
 #include <sys/syscall.h> 
 #include <unistd.h>
 
-uint64_t byte_sort(uint64_t arg) {
+int main (void) {
+    byte_sort(0x0403deadbeef0201);
     return 0;
+}
+
+
+uint64_t byte_sort(uint64_t arg) {
+    uint8_t firstByte = (arg) & 0xff;
+    uint8_t secondByte = (arg >> 8) & 0xff;
+    uint8_t thirdByte = (arg >> 16) & 0xff;
+    uint8_t fourthByte = (arg >> 24) & 0xff;
+    uint8_t fifthByte = (arg >> 32) & 0xff;
+    uint8_t sixthByte = (arg >> 40) & 0xff;
+    uint8_t seventhByte = (arg >> 48) & 0xff;
+    uint8_t eighthByte = (arg >> 56) & 0xff;
+
+
+    return firstByte + secondByte + thirdByte + fourthByte + fifthByte + sixthByte + seventhByte + eighthByte;
 }
 
 uint64_t nibble_sort(uint64_t arg) {
